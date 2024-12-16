2da0c5aeeb68d88f8d09d9e6f6142afd804a6f01
Alex Zhang <alex.lx.zhang@gmail.com>
2024-12-15 21:40:35 -0800

    refactor: remove wrapped try catch logic, and clean up. remove prints

--- src/discord-cluster-manager/leaderboard_db.py
+++ src/discord-cluster-manager/leaderboard_db.py
@@ -58,7 +58,7 @@ class LeaderboardDB:
         """Context manager exit"""
         self.disconnect()
 
-    def create_leaderboard(self, leaderboard: LeaderboardItem):
+    def create_leaderboard(self, leaderboard: LeaderboardItem) -> Optional[None]:
         try:
             self.cursor.execute(
                 """
@@ -75,6 +75,8 @@ class LeaderboardDB:
         except psycopg2.Error as e:
             print(f"Error during leaderboard creation: {e}")
             self.connection.rollback()  # Ensure rollback if error occurs
+            return "Error during leaderboard creation."
+        return None
 
     def create_submission(self, submission: SubmissionItem):
         try:
@@ -98,7 +100,9 @@ class LeaderboardDB:
             self.connection.rollback()  # Ensure rollback if error occurs
 
     def get_leaderboards(self) -> list[LeaderboardItem]:
-        self.cursor.execute("SELECT id, name, deadline, reference_code FROM leaderboard.problem")
+        self.cursor.execute(
+            "SELECT id, name, deadline, reference_code FROM leaderboard.problem"
+        )
 
         return [
             LeaderboardItem(id=lb[0], name=lb[1], deadline=lb[2], reference_code=lb[3])
@@ -108,7 +112,7 @@ class LeaderboardDB:
     def get_leaderboard(self, leaderboard_name: str) -> int | None:
         self.cursor.execute(
             "SELECT id, name, deadline, reference_code FROM leaderboard.problem WHERE name = %s",
-            (leaderboard_name,)
+            (leaderboard_name,),
         )
 
         res = self.cursor.fetchone()
