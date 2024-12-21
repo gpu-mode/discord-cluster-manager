4518c794ad861a802c85851d5474d9cc7798a132
Alex Zhang <alex.lx.zhang@gmail.com>
2024-12-21 12:55:36 -0500

    fix: siros suggestions, remove dim and assert on implementation match

--- src/discord-cluster-manager/leaderboard_eval.py
+++ src/discord-cluster-manager/leaderboard_eval.py
@@ -9,18 +9,19 @@ from reference import ref_kernel, generate_input
 from train import custom_kernel
 
 
-def check_implementation():
+def check_implementation() -> bool:
     for _ in range(10):  # check multiple times
         input_tensors = generate_input()
         for input in input_tensors:
-            custom_output = custom_kernel(input, dim=-1)
-            ref_output = ref_kernel(input, dim=-1)
+            custom_output = custom_kernel(input)
+            ref_output = ref_kernel(input)
 
             if not torch.allclose(custom_output, ref_output, atol=1e-5):
                 print('mismatch found! custom implementation doesnt match reference.')
-                return
+                return False
 
     print('custom implementation matches the reference implementation.')
+    return True
 
 
 def metric():
@@ -32,8 +33,8 @@ def metric():
     for _ in range(warmup_runs):
         input_tensors = generate_input()
         for input in input_tensors:
-            _ = custom_kernel(input, dim=-1)
-            _ = ref_kernel(input, dim=-1)
+            _ = custom_kernel(input)
+            _ = ref_kernel(input)
 
     # timing
     print('timing custom implementation...')
@@ -41,7 +42,7 @@ def metric():
     start_time = time.time()
     for _ in range(timed_runs):
         for input in input_tensors:
-            _ = custom_kernel(input, dim=-1)
+            _ = custom_kernel(input)
 
     custom_duration = (time.time() - start_time) / timed_runs
 
@@ -50,7 +51,7 @@ def metric():
     return custom_duration
 
 def main():
-    check_implementation()
+    assert (check_implementation())
     s = metric()
 
     print(f'score:{s}')
