3ec0584e41a98ee2b667eaa3edf26ec476c1a9f5
Erik Schultheis <erik.schultheis@aalto.fi>
2025-01-11 14:50:30 +0200

    exit on evaluation failure

--- src/discord-cluster-manager/eval.cu
+++ src/discord-cluster-manager/eval.cu
@@ -49,8 +49,9 @@ float measure_runtime() {
 
         auto reference_output = ref_kernel(data);
         if (!check_implementation(submission_output, reference_output)) {
-            std::cout << "check_implementation failed" << std::endl;
-            return 1;
+            std::cerr << "check_implementation failed" << std::endl;
+            // following pytest convention, code 1 means that tests failed
+            std::exit(1);
         }
 
     }
