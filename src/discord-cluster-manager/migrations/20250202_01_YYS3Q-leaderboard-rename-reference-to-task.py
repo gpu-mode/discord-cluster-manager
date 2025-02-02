6cc6655338b8347aa3608254c457e815486f5f7f
S1ro1 <matej.sirovatka@gmail.com>
2025-02-02 23:50:11 +0100

    Feat: add dropping old entries to the migration

--- src/discord-cluster-manager/migrations/20250202_01_YYS3Q-leaderboard-rename-reference-to-task.py
+++ src/discord-cluster-manager/migrations/20250202_01_YYS3Q-leaderboard-rename-reference-to-task.py
@@ -7,6 +7,9 @@ from yoyo import step
 __depends__ = {"20250106_01_Sgph3-add-leaderboard-creator-id"}
 
 steps = [
+    step(
+        "DELETE FROM leaderboard.leaderboard;"
+    ),  # Delete all entries as these are `breaking` changes
     step("ALTER TABLE leaderboard.leaderboard RENAME COLUMN reference_code TO task;"),
     step("ALTER TABLE leaderboard.leaderboard ALTER COLUMN task TYPE JSONB USING task::jsonb;"),
 ]
