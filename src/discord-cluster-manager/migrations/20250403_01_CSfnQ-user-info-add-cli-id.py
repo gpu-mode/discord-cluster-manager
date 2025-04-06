31cfc0b777b50422d4ebae26818dd6415305ab75
S1ro1 <matej.sirovatka@gmail.com>
2025-04-06 18:59:56 +0200

    Fix: new migration to keep linear history

--- src/discord-cluster-manager/migrations/20250403_01_CSfnQ-user-info-add-cli-id.py
+++
@@ -1,9 +0,0 @@
-"""
-user-info-add-cli-id
-"""
-
-from yoyo import step
-
-__depends__ = {"20250316_01_5oMi3-remember-forum-id"}
-
-steps = [step("ALTER TABLE leaderboard.user_info ADD COLUMN cli_id VARCHAR(255) DEFAULT NULL;")]
